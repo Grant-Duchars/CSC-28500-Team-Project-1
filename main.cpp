55ce6e88c31cf309305a13582be46d6d6f0dd606
Romyal Swarnkar <50716582+romyl25@users.noreply.github.com>
2022-10-08 00:56:13 -0500

    Uploading a separate cpp file
    This file has the last two conversion implementations

--- main.cpp
+++ main.cpp
@@ -1,153 +1,134 @@
-/*
-Team Name: XXXXXX
-Team Members: Grant Duchars,
-October 10, 2022 - Applied Discrete Structures, Fall 2022, Dr. J
-
-Description: A menu-driven program to convert user entered numbers
-Possible convertions include: Decimal to Binary,
-                              Hexadecimal to Binary,
-                              Binary to Decimal,
-                              Binary to Hexadecimal
-*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <iostream>
+#include <unordered_map>
 
-void convertDecimalToBinary(char *input);
-void convertDecimalToBinaryHelper(int num, char *result);
-void convertBinaryToDecimal(char *input);
+using namespace std;
 
-int main()
-{
-    // Clear out terminal output
-    printf("\033[1;1H\033[2J\n\n\n");
-    while (true)
-    {
-        // Prompt user with menu
-        printf("\033[1;35mCatchy Name Conversion Program\n\n\033[0;36m1. Convert base 10 to binary\n2. Convert binary to base 10\n3. Convert base 16 to binary\n4. Convert binary to base 16\n5. End program\n\n\033[33mPlease enter your selection:\033[0m ");
-        char *inputBuffer = (char *)malloc(32);
-        char *numBuffer = (char *)malloc(32);
-        // Get user input
-        fgets(inputBuffer, sizeof(inputBuffer), stdin);
-        // Remove new line character from user input
-        inputBuffer[strcspn(inputBuffer, "\n")] = 0;
-        // Check which option user selected
-        if (inputBuffer == NULL) // ERROR: No command entered
-        {
-            printf("\033[1;1H\033[2J\033[4;31m\nERROR: Please enter a number from the list\033[0m\n\n");
+void binaryToHex(string);
+void hexToBinary(string);
+
+int main(int argc, char **argv) {
+    
+    string str;
+    cout << "Enter hexadecimal to be converted to binary (Enter a space between every number)" <<endl;
+    //cout << "Enter binary to be converted to hexadecimal (Enter in four bits and with a space between every four bits)" <<endl;
+    getline(cin, str); 
+    
+    hexToBinary(str);
+    //binaryToHex(str);
+    
+    return 0;
+}
+
+void binaryToHex(string input) {
+    
+    unordered_map<string, string> m; //A map storing the conversion values.
+
+    m["0000"] = "0";
+    m["0001"] = "1";
+    m["0010"] = "2";
+    m["0011"] = "3";
+    m["0100"] = "4";
+    m["0101"] = "5";
+    m["0110"] = "6";
+    m["0111"] = "7";
+    m["1000"] = "8";
+    m["1001"] = "9";
+    m["1010"] = "A";
+    m["1011"] = "B";
+    m["1100"] = "C";
+    m["1101"] = "D";
+    m["1110"] = "E";
+    m["1111"] = "F";
+
+    char del = ' ';         //character delimiter
+     
+    string temp = "";       
+    string temp1 = "";
+    
+    //cout << input[2] <<endl;
+    
+    for(int i=0; i<(int)input.size(); i++) {
+    // If current char is not delimiter (space in this case), then append it to the "temp" string, then look in the map for it's equivalent hex value and append it to "temp1" string. 
+    // Otherwise you have completed the word, print it, and then start a new word.
+    
+        if (input[i] != '0' && input[i] != '1' && input[i] != ' ') {        //Checking for valid input
+            cout << "Enter a valid binary number" <<endl;
         }
-        else if (strcmp(inputBuffer, "1") == 0) // Convert Decimal to Binary
-        {
-            printf("\033[1;1H\033[2J\033[33m\nPlease enter your base 10 number to convert to binary:\033[0m ");
-            fgets(numBuffer, sizeof(numBuffer), stdin);
-            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
-            convertDecimalToBinary(numBuffer);
+        
+        else if (input[i] != del){
+            temp += input[i];
+            temp1 += m[temp];
         }
-        else if (strcmp(inputBuffer, "2") == 0) // Convert Binary to Decimal
-        {
-            printf("\033[1;1H\033[2J\033[33m\nPlease enter your binary number to convert to base 10:\033[0m ");
-            fgets(numBuffer, sizeof(numBuffer), stdin);
-            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
-            convertBinaryToDecimal(numBuffer);
+        
+        else{
+            if (temp.size() != 4) {                                         //Checking if the binary number is entered in four bits
+                cout << "The Binary number is not in 4 bits" <<endl;
+            }
+            else {
+                cout << temp1 << " ";
+                temp = "";
+                temp1 = "";
+                
+            }
         }
-        else if (strcmp(inputBuffer, "3") == 0) // Convert Hexadecimal to Binary
-        {
-            printf("\033[1;1H\033[2J\033[33m\nPlease enter your base 16 number to convert to binary:\033[0m ");
-            fgets(numBuffer, sizeof(numBuffer), stdin);
-            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
-            // TODO: convertHexToBinary(numBuffer);
-        }
-        else if (strcmp(inputBuffer, "4") == 0) // Convert Binary to Hexadecimal
-        {
-            printf("\033[1;1H\033[2J\033[33m\nPlease enter your binary number to convert to base 16:\033[0m ");
-            fgets(numBuffer, sizeof(numBuffer), stdin);
-            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
-            // TODO: convertBinaryToHex(numBuffer);
-        }
-        else if (strcmp(inputBuffer, "5") == 0) // Exit the program
-        {
-            printf("\033[1;1H\033[2J\033[1;32m\nThank you for using Catchy Name Conversion Program\033[0m\n\n");
-            return 0;
-        }
-        else // ERROR: Wrong command entered
-        {
-            printf("\033[1;1H\033[2J\033[4;31m\nERROR: Please enter a number from the list\033[0m\n\n");
-        }
-        free(numBuffer);
-        free(inputBuffer);
+    }
+    
+    if (temp.size() != 4) {
+        cout << "The Binary number is not in 4 bits" <<endl;
+    }
+    
+    else {
+        cout << temp1 <<endl;
     }
 }
 
-/**
- * Function to convert a decimal integer to a binary integer
- * @param input A string of the number you wish to convert
- */
-void convertDecimalToBinary(char *input)
-{
-    // Convert string to integer
-    int num = atoi(input);
-    // Allocate space for result string
-    char *result = (char *)malloc(32);
-    // Clear out the result string
-    strcpy(result, "");
-    // Recursively run the function until num is zero
-    convertDecimalToBinaryHelper((num / 2), result);
-    // After recursion append last digit
-    (num % 2) ? strcat(result, "1") : strcat(result, "0");
-    // Print out the binary number result
-    printf("\033[1;1H\033[2J\033[1;4;32m\nYour binary number is:\033[0m %s\n\n", result);
-    // FREE THE MEMORY
-    free(result);
-}
 
-/**
- * Helper method for convertDecimalToBinary()
- */
-void convertDecimalToBinaryHelper(int num, char *result)
-{
-    // If num is not zero
-    if (num)
-    {
-        // Recursively run the function until num is zero
-        convertDecimalToBinaryHelper((num / 2), result);
-        // On the way up append the correct digit to result string
-        (num % 2) ? strcat(result, "1") : strcat(result, "0");
-    }
-}
+void hexToBinary(string input) {
+    
+    unordered_map<string, string> m;
+    
+    m["0"] = "0000";
+    m["1"] = "0001";
+    m["2"] = "0010";
+    m["3"] = "0011";
+    m["4"] = "0100";
+    m["5"] = "0101";
+    m["6"] = "0110";
+    m["7"] = "0111";
+    m["8"] = "1000";
+    m["9"] = "1001";
+    m["A"] = "1010";
+    m["B"] = "1011";
+    m["C"] = "1100";
+    m["D"] = "1101";
+    m["E"] = "1110";
+    m["F"] = "1111";
 
-/**
- * Function to convert a binary integer to a decimal integer
- * @param input A string of the number you wish to convert
- */
-void convertBinaryToDecimal(char *input)
-{
-    // Initialize decimal result variable
-    int result = 0;
-    // Calculate most significant bit's power
-    int power = strlen(input) - 2;
-    // Loop through binary number string
-    for (int i = 0; input[i] != '\0'; ++i)
-    {
-        // Only care about ones in binary number string
-        if (input[i] == '1')
-        {
-            // Add two^power of the current bit to result
-            result += pow(2, power);
+    char del = ' '; 
+     
+    string temp = "";
+    string temp1 = "";
+
+    for(int i=0; i<(int)input.size(); i++) {
+    // If current char is not delimiter (space in this case), then append it to the "temp" string, then look in the map for it's equivalent binary value and append it to "temp1" string. 
+    // Otherwise you have completed the word, print it, and then start a new word.
+    
+        if (!isxdigit(input[i]) && input[i] != ' '  )  {        //Checking for valid input
+            cout << "Enter a valid Hexadecimal number" <<endl;
         }
-        // Decrement power of current bit by one
-        --power;
-    }
-    // Print out the decimal number result
-    printf("\033[1;1H\033[2J\033[1;4;32m\nYour decimal number is:\033[0m %d\n\n", result);
+    
+        else if(input[i] != del){
+            temp += input[i];
+            temp1 += m[temp];
+        }
+        else{
+            cout << temp1 << " ";
+            temp = "";
+            temp1 = "";
+        }
+    } 
+
+    cout << temp1 <<endl;
 }
 
-void convertHexToBinary(char num[])
-{
-    // TODO: Implement this
-}
-
-void convertBinaryToHex(char num[])
-{
-    // TODO: Implement this
-}
